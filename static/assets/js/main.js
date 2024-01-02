2f3b942e71582958e53b0e8887380f04fa029613
Peniel Oputa <61382527+payniel@users.noreply.github.com>
2024-01-01 23:03:08 -0500

    Update main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -16,6 +16,7 @@
 			normal:    [ '961px',  '1620px' ],
 			narrow:    [ '961px',  '1320px' ],
 			narrower:  [ '737px',  '960px'  ],
+			mobile:    [ null,     '736px'  ]
 		});
 
 	// Play initial animations on page load.
